b7c4010062d6c263ce74e2bb5d0b3b1bbc8c9b55
RegulusXiao <1048151708@qq.com>
2018-08-31 00:04:52 +0800

    revise

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PersonalRepository
-PersonalRepository
+Personal Files
+This is My First Time Using GitHub, It's cool !
---
+++ workspace/go/LearningDemos/chapter1.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main()  {
+	// first test, hello wrold
+	fmt.Println("Hello, 世界")
+
+	// for range, if we don't use index, may be error!Placeholder is charactor '_'
+	for index, argv := range os.Args {
+		fmt.Println(index, argv)
+	}
+
+	
+}
